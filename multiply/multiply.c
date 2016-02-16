d42a56a010a0f3bd542628bb8fbc26eda0627cd2
Jimmy Holm <senilica@gmail.com>
2016-02-16 14:22:04 +0100

    -

--- multiply/multiply.c
+++ multiply/multiply.c
@@ -45,11 +45,11 @@ matmul_opt()
          */
         int i, j, k;
 
-        for (j = 0; j < SIZE; j+=2) {
+        for (j = 0; j < SIZE; j++) {
             for (i = 0; i < SIZE; i++) {
-                for (k = 0; k < SIZE; k++) {
+                for (k = 0; k < SIZE; k+=2) {
                     mat_c[i][j] += mat_a[i][k] * mat_b[k][j];
-                    mat_c[i][j+1] += mat_a[i][k] * mat_b[k][j+1];
+                    mat_c[i][j] += mat_a[i][k+1] * mat_b[k+1][j];
                 }
             }
         }
